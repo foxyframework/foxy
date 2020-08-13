703ba25dc5ee80728e3809e5d85d2121b726978d
jprojects <kim@aficat.com>
2020-08-13 13:46:19 +0200

    some fixes and new ajaxCall function

--- template/system/js/foxy.js
+++ template/system/js/foxy.js
@@ -17,6 +17,18 @@ function getCookie(c_name) {
     return "";
 }
 
+function ajaxCall(url, callback) {
+	var xhttp;
+	xhttp=new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+		callback(this);
+	  }
+	};
+	xhttp.open("GET", url, true);
+	xhttp.send();
+  }
+
 function deleteAccount(username, domain) {
 	if(document.getElementById('proceed').value.toLowerCase() == username) {
 		document.location.href = domain+'?view=config&task=deleteAccount';
@@ -32,57 +44,60 @@ function getParameterByName(name) {
 	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-var myTable = document.getElementById("datatable");
-if(myTable != 'undefined' && myTable != 'null') {
-	var table = new DataTable(myTable);
-}
+document.addEventListener("DOMContentLoaded", function() {
 
-var editor = document.getElementById("editor");
-if(editor != 'undefined' && editor != 'null') {
-    CKEDITOR.replace( 'editor' );
-}
+	var myTable = document.getElementById("datatable");
+	if(typeof(myTable) != 'undefined' && myTable != 'null') {
+		var table = new DataTable(myTable);
+	}
 
-//delete
-if(document.getElementsByClassName('.btn_delete').lenght) {
-	document.querySelector('btn_delete').addEventListener("click",function(e) {
+	var editor = document.getElementById("editor");
+	if(typeof(editor) != 'undefined' && editor != 'null') {
+	    const editor = CKEDITOR.replace( 'editor' );
+	}
 
-		e.preventDefault();
-		var items = [];
+	//delete
+	if(document.getElementsByClassName('.btn_delete').lenght) {
+		document.querySelector('btn_delete').addEventListener("click",function(e) {
 
-		$(':checkbox').each(function(el) {
-		if(this.checked) {
-				var id    = el.target.getAttribute('data-id');
-				items.push(id);
+			e.preventDefault();
+			var items = [];
+
+			$(':checkbox').each(function(el) {
+			if(this.checked) {
+					var id    = el.target.getAttribute('data-id');
+					items.push(id);
+				}
+			});
+
+			var view = e.target.getAttribute('data-view');
+			var pageURL = e.target.getAttribute("href");
+
+			if(items == 0) { alert('Please check one item at least'); return false; } else { if(!confirm('Are you sure you want to delete this item?')) return false; }
+
+			var list = JSON.stringify(items);
+
+			var ajax = new XMLHttpRequest();
+
+			ajax.open("POST", pageURL, true);
+			ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			ajax.send("items="+list);
+
+			// Cria um evento para receber o retorno.
+			ajax.onreadystatechange = function() {
+			
+			// Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
+				if (ajax.readyState == 4 && ajax.status == 200) {
+				
+					var data = ajax.responseText;
+					//Todo: display success message
+					items.forEach(item => {
+						document.querySelector(`[data-id="${item}"]`).remove();
+					})
+				} else {
+					//display error message
+				}
 			}
 		});
-
-		var view = e.target.getAttribute('data-view');
-		var pageURL = e.target.getAttribute("href");
-
-		if(items == 0) { alert('Please check one item at least'); return false; } else { if(!confirm('Are you sure you want to delete this item?')) return false; }
-
-		var list = JSON.stringify(items);
-
-		var ajax = new XMLHttpRequest();
-
-		ajax.open("POST", pageURL, true);
-		ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		ajax.send("items="+list);
-
-		// Cria um evento para receber o retorno.
-		ajax.onreadystatechange = function() {
-		
-		// Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
-			if (ajax.readyState == 4 && ajax.status == 200) {
-			
-				var data = ajax.responseText;
-				Messenger().post({message: view+' success deleted', type: 'success', hideAfter: 10});
-				items.forEach(item => {
-					document.querySelector(`[data-id="${item}"]`).remove();
-				})
-			} else {
-				Messenger().post({message: 'Sembla que tenim algun problema', type: 'error', hideAfter: 10});
-			}
-		}
-	});
-}
+	}
+});
