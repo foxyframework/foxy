b6296c70fa4bd0f09ca8dc8e08d6d95cfdfefb8a
foxyframework <kim+foxy@aficat.com>
2020-09-03 18:03:54 +0200

    link to dashboard admin

--- template/admin/index.php
+++ template/admin/index.php
@@ -70,12 +70,17 @@
         <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
         <div class="position-sticky pt-3">
             <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php?view=admin" aria-expanded="false">
+                <span>Dashboard</span>
+              </a>
+            </li>
             <?php foreach($model->getAdminViews() as $view) : ?>
                 <li class="nav-item">
-                <a class="nav-link" href="index.php?view=<?= $view; ?>&layout=admin" aria-expanded="false">
-                    <span><?= ucfirst($view); ?></span>
-                </a>
-            </li>
+                  <a class="nav-link" href="index.php?view=<?= $view; ?>&layout=admin" aria-expanded="false">
+                      <span><?= ucfirst($view); ?></span>
+                  </a>
+                </li>
             <?php endforeach; ?>
             </ul>
         </div>
