9f7c6551e50a58ad777ebd78a5e227c1909ede78
foxyframework <kim+foxy@aficat.com>
2020-09-08 14:53:54 +0200

    login via plugin

--- component/models/register.php
+++ component/models/register.php
@@ -266,45 +266,7 @@ class register extends model
     {
         if(parent::allowTask(__FUNCTION__, $_GET['task'])) {
 
-            $email    = application::getVar('email', '', 'post', 'string');
-            $password = application::getVar('password', '', 'post', 'string');
-            $redirect = application::getVar('return', '', 'post', 'string');
-            $token    = application::getVar('token', '', 'post', 'string');
-
-            //si un campo esta vacio abortamos...
-            if($email == "" || $password == "") {
-                application::setMessage(language::get('Rellena todos los campos por favor'), 'danger');
-                application::redirect(config::$site.config::$login_redirect);
-                return false;
-            }
-
-            database::query("SELECT password FROM `#_users` WHERE email = ".database::quote($email)." AND block = 0");
-            $dbpass = database::loadResult();
-            if(application::decryptPassword($password, $dbpass)) {
-
-                database::query("SELECT id FROM `#_users` WHERE email = ".database::quote($email)." AND block = 0");
-                if($id = database::loadResult()) {
-                    user::setAuth($id);
-
-                    //register session
-                    session::createSession();
-
-                    database::updateField('#_users', 'lastvisitDate',  application::getVar('lastvisitDate'), 'id', $id);
-                    application::setMessage(language::replace('FOXY_LOGIN_SUCCESS_MSG',  $user->username), 'success');
-                    $redirect == '' ? $authUrl = config::$site.config::$login_redirect : $authUrl = base64_decode($redirect);
-                    application::redirect($authUrl);
-                    return true;
-
-                } else {
-                    application::setMessage(language::get('FOXY_LOGIN_ERROR_MSG'), 'danger');
-                    application::redirect(url::genUrl('/index.php?view=register&layout=login'));
-                    return false;
-                }
-            } else {
-                application::setMessage(language::get('Password not match'), 'danger');
-                application::redirect(url::genUrl('/index.php?view=register&layout=login'));
-                return false;
-            }
+            application::trigger('onLogin', $_POST);
         }
 
     }
